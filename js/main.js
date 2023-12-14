3f25baf8073c65198c1a9a2172939b45506de7f4
magomed_n <naurbaev2020@gmail.com>
2023-12-14 18:00:59 +0300

    Разметка и стили для игровой доски

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
 import "../assets/styles/style.css";
-
-document.querySelector("#board").innerHTML = `
-  <div>
-
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+import "../assets/styles/board.css";
